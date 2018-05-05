c44cbe15192a8404dc26a54448ce91092f7343a6
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-05 13:59:20 -0600

    builds for express

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://infinite-retreat-10591.herokuapp.com";
+	    return "https://evening-reef-62953.herokuapp.com/";
 	  }
 	};
 
