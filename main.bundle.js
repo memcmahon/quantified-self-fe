364e752f58884ffed5f92a122a7c757a36b299c3
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-09 16:22:46 -0600

    builds with phoenix url

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://evening-reef-62953.herokuapp.com";
+	    return "https://frozen-garden-41922.herokuapp.com";
 	  }
 	};
 
